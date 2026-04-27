ed4ff2e981bcadaade4c2964f722b602cd887391
Arrow0ne <arturwacker7@gmail.com>
2026-04-27 11:36:44 +0200

    Lektion 17 Exceptions

---
+++ src/main/java/de/thws/lektion17/Exceptions.java
@@ -0,0 +1,34 @@
+package de.thws.lektion17;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+public class Exceptions {
+    public static void main(String[] args) {
+
+//    a) NumberFormatException
+//        int a = Integer.parseInt("123a");
+//    b) ArrayIndexOutOfBoundsException
+//        int[] b = new int[1];
+//
+//        for (int i = 0; i <= b.length; i++) {
+//            b[i] = 0;
+//        }
+//    c) NullPointerException
+//        String s = null;
+//        s.length();
+//    d) OutOfMemoryError
+//        List<String> list = new ArrayList<>();
+//
+//        while (true) {
+//            list.add("Hello World"); // keeps adding, never stops
+//        }
+//    e) FileNotFoundException
+//        FileReader file = new FileReader("hello.txt");
+//    f) ArithmeticException
+//        int a = 10;
+//        int b = 0;
+//
+//        int result = a / b;
+    }
+}
